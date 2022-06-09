e4a70b970db5c788494854a8162c6a2ebd6dda50
alphanse <alphanse98@gmail.com>
2022-06-09 09:52:02 +0530

    add-file

---
+++ script.js
@@ -0,0 +1,9 @@
+let a = parseInt(prompt('Enter a number a'))
+let b = parseInt(prompt('Enter a number b'))
+let c = parseInt(prompt('Enter a number c'))
+ d = (b*b) - (4*a*c)
+
+ r1 = (-b + Math.sqrt(d)) / (2 * a);
+ r2 = (-b - Math.sqrt(d)) / (2 * a);
+    
+      console.log('ans is',r1,"and" ,r2)
